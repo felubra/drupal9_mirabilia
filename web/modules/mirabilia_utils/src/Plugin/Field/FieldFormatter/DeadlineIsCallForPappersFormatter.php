ee3af25bcf98b8551c7121d89056a0d1429ae0e8
Felipe <felipe.lubra@gmail.com>
2021-10-16 17:16:50 -0300

    fix: consider the number of articles of a issue
    The deadline formatter should consider an Issue as accepting
    papers if the quantity of accepted papers is zero.

--- web/modules/mirabilia_utils/src/Plugin/Field/FieldFormatter/DeadlineIsCallForPappersFormatter.php
+++ web/modules/mirabilia_utils/src/Plugin/Field/FieldFormatter/DeadlineIsCallForPappersFormatter.php
@@ -4,6 +4,8 @@ namespace Drupal\mirabilia_utils\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Entity\EntityInterface;
+use Exception;
 
 /**
  * Plugin implementation of the 'Utils' formatter.
@@ -19,20 +21,48 @@ use Drupal\Core\Field\FormatterBase;
 class DeadlineIsCallForPappersFormatter extends FormatterBase
 {
 
+  private function getArticlesCountForIssue(EntityInterface $entity) {
+    $issue_type = $entity->bundle();
+    $nid  = $entity->id();
+
+    $articleType = '';
+    switch ($issue_type) {
+      case 'issue': {
+        $articleType = 'article';
+        break;
+      }
+    }
+
+    if (!empty($articleType)) {
+      try {
+        $query =  \Drupal::entityQuery('node')
+          ->condition('type', $articleType)
+          ->condition('field_'.$issue_type.'.entity.nid', $nid);
+
+        return ($query->count()->execute()) * 1;
+      } catch(Exception $e) {
+        return 0;
+      }
+    }
+    return 0;
+  }
+
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode)
   {
     $element = [];
+    $entity = $items->getParent()->getEntity();
 
     foreach ($items as $delta => $item) {
+      $issue_type = $item->parent->parent->entity->entityKeys["bundle"];
 
       try {
         $deadline = new \DateTime($item->value);
         $today = new \DateTime();
 
-        $value = $today->diff($deadline)->days <= 1
+        $value = ($today->diff($deadline)->days <= 1) || ($this->getArticlesCountForIssue($entity) == 0)
           ? t('Issues accepting papers')
           : t('Past issues');
       } catch (\Exception $e) {
